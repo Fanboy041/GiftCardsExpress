39b0df583e1109fcab5421815b2a466434d01922
wal6ed <waleed6.kahale@gmail.com>
2025-01-07 03:16:16 +0100

    edit shop function

--- src/Callbacks/GiftCards/shop.py
+++ src/Callbacks/GiftCards/shop.py
@@ -1,34 +1,31 @@
 # callback to show shop balance
 from telebot import types
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
-from Callbacks import servicesNameAsButtons
-from Database.GiftCards import get_gift_cards_price
+from Database.GiftCards import get_gift_cards_price, get_gift_card_code_by_price
+from Database.Users import get_wallet, update_wallet
 
-#TODO: get prices to all services as String
 
 def handle_shop_callback(call, bot):
-
     service_name = call.data.split('_', 1)[0]
-    print(service_name)
     gift_card_prices = get_gift_cards_price(service_name)
-    print(gift_card_prices)
-    chat_id=call.message.chat.id
-    message_id=call.message.message_id
+    chat_id = call.message.chat.id
+    message_id = call.message.message_id
+
     prices_button = []
     gift_card_price_keyboard = []
-    back_button_keyboard = []
+    back_button_keyboard = [types.InlineKeyboardButton("Back 🔙", callback_data='shop')]
 
-    back_button_keyboard.append(types.InlineKeyboardButton("Back 🔙", callback_data='back_to_shop_menu'))
-    back_keybaord = types.InlineKeyboardMarkup([back_button_keyboard])
+    back_keyboard = types.InlineKeyboardMarkup([back_button_keyboard])
 
-    if gift_card_prices == []:
-        print("No gift cards available")
-        bot.edit_message_text("No gift cards available\n\nIf you want to request a gift card please let me know", chat_id, message_id, reply_markup=back_keybaord)
+    if not gift_card_prices:
+        bot.edit_message_text("No gift cards available\n\nIf you want to request a gift card please let me know",
+                              chat_id, message_id, reply_markup=back_keyboard)
         return
 
     for gift_card_price in gift_card_prices:
-        prices_button.append(f"{gift_card_price}_{call.data}")
-        gift_card_price_keyboard.append(InlineKeyboardButton(gift_card_price, callback_data=f"{gift_card_price}_{call.data}"))
+        prices_button.append(f"{gift_card_price}_{service_name}")
+        gift_card_price_keyboard.append(InlineKeyboardButton(gift_card_price,
+                                                             callback_data=f"{gift_card_price}_{service_name}"))
 
     shop_keyboard = [
         gift_card_price_keyboard,
@@ -38,3 +35,22 @@ def handle_shop_callback(call, bot):
     keyboard = InlineKeyboardMarkup(shop_keyboard)
     bot.edit_message_text(f"Choose your Price:\n\n", chat_id,
                           message_id, reply_markup=keyboard, parse_mode='HTML')
+
+    @bot.callback_query_handler(func=lambda call: call.data in prices_button)
+    def handle_yes_callback(call):
+        price = float(call.data.split('_')[0])
+        try:
+            if price <= get_wallet(chat_id):
+                text = update_wallet(chat_id, price)
+                gift_card_code = get_gift_card_code_by_price(call.data.split('_')[1], price)
+                message_text = f"The {service_name} key is: {gift_card_code}\n\n {text}"
+            else:
+                message_text = "You must add Balance to your wallet"
+
+            bot.edit_message_text(f"{message_text}", chat_id, message_id,
+                                  reply_markup=back_keyboard, parse_mode='HTML')
+
+        except Exception as e:
+            # Handle any errors that occur during message deletion or sending
+            print(f"Error handling confirm callback: {str(e)}")
+            raise
