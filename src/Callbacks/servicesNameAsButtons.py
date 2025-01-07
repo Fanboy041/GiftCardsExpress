c0eae0cb0c68a7ac50fbdf3d7bff159c63e10c69
wal6ed <waleed6.kahale@gmail.com>
2025-01-07 03:15:23 +0100

    make the buttons 4 in row

--- src/Callbacks/servicesNameAsButtons.py
+++ src/Callbacks/servicesNameAsButtons.py
@@ -14,7 +14,6 @@ def handle_services_name_as_buttons_callback(call, bot):
     # Make inline keyboard
     service_name_keyboard = []
     add_service_name_keyboard = []
-    types.InlineKeyboardMarkup(row_width=2)
     for service_name in services_name:
         services_button.append(f"{service_name}_{call.data}")
         service_name_keyboard.append(InlineKeyboardButton(service_name, callback_data=f"{service_name}_{call.data}"))
@@ -26,12 +25,15 @@ def handle_services_name_as_buttons_callback(call, bot):
 
     start_keyboard = [
         service_name_keyboard,
-        add_service_name_keyboard
+        add_service_name_keyboard,
     ]
+    keyboard = InlineKeyboardMarkup(row_width=4)
 
-    keyboard = InlineKeyboardMarkup(start_keyboard)
-    bot.edit_message_text(f"Choose your Service:\n\n", chat_message_id,
-                          call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
+    for button in start_keyboard:
+        for i in range(0, len(button), 4):
+            keyboard.add(*button[i:i + 4])
+            bot.edit_message_text(f"Choose your Service:\n\n", chat_message_id,
+                                  call.message.message_id, reply_markup=keyboard, parse_mode='HTML')
 
     # Services Callback
     @bot.callback_query_handler(func=lambda call: call.data in services_button)
@@ -39,13 +41,11 @@ def handle_services_name_as_buttons_callback(call, bot):
         service_name_button = call.data.split('_', 1)
         match service_name_button[1]:
             case 'add_gift_card':
-                # if 'add_gift_card' == service_name_button[1]:
                 addGiftCardCode.handle_add_gift_card_code_callback(call, bot)
             case 'shop':
                 shop.handle_shop_callback(call, bot)
-            case 'back_to_shop_menu':
-                shop.handle_shop_callback(call, bot)
             case 'show_gift_card':
+                #TODO: I don't know
                 print(get_services_name())
             case _:
                 return "Something's wrong with the internet"
